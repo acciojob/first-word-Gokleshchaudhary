db36f1714d84462bd43e1588ec5290d8ed07f3ab
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-04-29 16:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+    if (s.length === 0) {
+        return '';
+    }
+    
+    const spaceIndex = s.indexOf(' ');
+    
+    if (spaceIndex === -1) {
+        return s; // Return the entire string if no space is found
+    }
+    
+    return s.substring(0, spaceIndex); // Return the substring up to the first space
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+function showFirstWord() {
+    const inputString = document.getElementById('inputString').value;
+    const result = firstWord(inputString);
+    document.getElementById('result').innerText = `First Word: '${result}'`;
+}
