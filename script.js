c9545f5f2f52382e713527a7831f463b107429c0
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-04-29 16:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-function firstWord(s) {
-    if (s.length === 0) {
-        return '';
-    }
-    
-    const spaceIndex = s.indexOf(' ');
-    
-    if (spaceIndex === -1) {
-        return s; // Return the entire string if no space is found
-    }
-    
-    return s.substring(0, spaceIndex); // Return the substring up to the first space
-}
-
 function showFirstWord() {
     const inputString = document.getElementById('inputString').value;
+    console.log("Input String:", inputString); // Check what input is received
     const result = firstWord(inputString);
+    console.log("First Word:", result); // Check what first word is returned
     document.getElementById('result').innerText = `First Word: '${result}'`;
 }
